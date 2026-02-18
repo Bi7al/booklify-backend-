da2e14969d47a2373dff4156ca5f9a24058ff9ec
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2026-02-18 20:53:44 +0500

    Added ENV VARS

--- api/analyze.js
+++ api/analyze.js
@@ -62,7 +62,7 @@ export default async function handler(req, res) {
 
 
 
-        const genAI = new GoogleGenerativeAI("AIzaSyDjCDno_4jTGKIUvLzJ_tBzlfN3TmEaeDQ");
+        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
         const result = await model.generateContent([
