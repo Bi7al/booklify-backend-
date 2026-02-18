9f0dbaf50175494e26cc218089b78388c687181a
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2026-02-18 20:41:57 +0500

    CORS Policiy

--- api/analyze.js
+++ api/analyze.js
@@ -25,11 +25,11 @@ export default async function handler(req, res) {
 
 
 
-    res.setHeader("Access-Control-Allow-Origin", "*"); // or your frontend URL
+    res.setHeader("Access-Control-Allow-Origin", "https://booklify-frontend.vercel.app"); // or your frontend URL
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-
+    //Handling Pre-Flight Requests
     if (req.method === "OPTIONS") {
         return res.status(200).end();
     }
