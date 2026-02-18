a027a71b873645c9d49ebf01f0dd841bb1a13751
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2026-02-18 20:51:13 +0500

    Booklify Backend 1.1.1

--- api/analyze.js
+++ api/analyze.js
@@ -63,7 +63,7 @@ export default async function handler(req, res) {
 
 
         const genAI = new GoogleGenerativeAI("AIzaSyDjCDno_4jTGKIUvLzJ_tBzlfN3TmEaeDQ");
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
         const result = await model.generateContent([
             prompt,
