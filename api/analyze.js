33ae05dd881512d73c2cb775d10b3587543480ba
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2026-02-19 15:47:45 +0500

    Changed MOdel for gemini

--- api/analyze.js
+++ api/analyze.js
@@ -63,7 +63,8 @@ export default async function handler(req, res) {
 
         // 
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-pro" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
+
 
         const result = await model.generateContent([
             prompt,
